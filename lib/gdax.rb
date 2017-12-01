7f1ec65cf3db3a301c7cb9f7cffce8ef1f55cfdc
Andrew Rempe <arempe@k2p.com>
2017-11-30 19:48:43 -0500

    add configuration options

--- lib/gdax.rb
+++ lib/gdax.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 require 'gdax/version'
 
 #
 # A Ruby client to GDAX REST API
 #
 module GDAX
+  @api_base = 'https://api.gdax.com'
+
+  @api_key = ENV['CB_ACCESS_KEY']
+  @api_signature = ENV['CB_ACCESS_SIGN']
+  @api_passphrase = ENV['CB_ACCESS_PASSPHRASE']
+
+  @logger = Logger.new(STDOUT)
+
+  class << self
+    attr_accessor :api_base, :api_key, :api_signature, :api_passphrase, :logger
+  end
 end
